eea6b04f1970e1f0aa201474feacbb2373d2ab67
camiloariasmoreno <camiloariasmoreno@gmail.com>
2026-05-21 20:53:45 -0500

    fix: Correcccion de la funcio calcular promedio

--- Promedioedades.py
+++ Promedioedades.py
@@ -2,10 +2,10 @@ def calcularpromediodeedad():
         cantidad_alumnos =int(input("Digite la cantidad de alumnos: "))
         suma_edades=0
         for i in range(1,cantidad_alumnos+1):
-            edad= float(input("digite la edad del alumno: "))
+            edad= float(input("digite la edad del alumno"))
             suma_edades=suma_edades+edad
 
-        promedio edad=suma_edades/cantidad_alumnos
+        promedioedad=suma_edades/cantidad_alumnos
+        print(f"El promedio de edades es: {promedioedad}")
         
-
-    calcularpromediodeedad()
+calcularpromediodeedad()
