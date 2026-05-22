7df08db6522c80deff6c110ba61f70f61812aea5
camiloariasmoreno <camiloariasmoreno@gmail.com>
2026-05-21 19:51:21 -0500

    feat:funcion promedio

---
+++ Promedioedades.py
@@ -0,0 +1,11 @@
+def calcularpromediodeedad():
+        cantidad_alumnos =int(input("Digite la cantidad de alumnos: "))
+        suma_edades=0
+        for i in range(1,cantidad_alumnos+1):
+            edad= float(input("digite la edad del alumno: "))
+            suma_edades=suma_edades+edad
+
+        promedio edad=suma_edades/cantidad_alumnos
+        
+
+    calcularpromediodeedad()
