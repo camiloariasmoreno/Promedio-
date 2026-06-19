d4d182831b93a44c2dd097e1099b0d1858deddcc
camiloariasmoreno <camiloariasmoreno@gmail.com>
2026-06-18 21:13:26 -0500

    feat:Adivinarnumero

---
+++ Adivinarnumero.py
@@ -0,0 +1,33 @@
+import random
+
+# Generar un número secreto entre 1 y 100
+numero_secreto = random.randint(1, 100)
+
+intentos = 0
+adivino = False
+
+while intentos < 6 and not adivino:
+
+    print(f"\nIntento {intentos + 1} de 6")
+
+    numero_usuario = int(input("Ingrese un número entre 1 y 100: "))
+
+    intentos += 1
+
+    if numero_usuario == numero_secreto:
+        print("¡Correcto!")
+        adivino = True
+
+    elif numero_usuario > numero_secreto:
+        print("El número es menor")
+
+    else:
+        print("El número es mayor")
+
+# Si no adivinó después de los 6 intentos
+if not adivino:
+    print(f"\nPerdiste, el número era {numero_secreto}")
+    
+
+
+
